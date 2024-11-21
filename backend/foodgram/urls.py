171715730c3fac800d08311ad161734ec2cb8160
Dinar Mullanurov <dinarmullanurov@yandex.ru>
2024-11-22 04:00:45 +0500

    third iteration fix short-link

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,5 +4,6 @@ from foodgram.views import ShortLinkViewSet
 
 urlpatterns = [
     path('s/<str:short_link>/',
-         ShortLinkViewSet.as_view(), name='short_link_redirect'),
+         ShortLinkViewSet.as_view({'get': 'short_link_redirect'}),
+         name='short-link-redirect'),
 ]
